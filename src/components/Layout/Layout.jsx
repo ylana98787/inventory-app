974a731eacfa2741ec216fec4ee47b11bdffd0b3
ylana98787 <ylana98787@gmail.com>
2026-04-21 20:02:27 +0300

    feat: додано компонент Layout

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+function Layout({ children }) {
+  return (
+    <div className="layout">
+      <header className="header">
+        <h1>🏭 Адмін-панель складу</h1>
+        <nav>
+          <Link to="/admin">📦 Інвентар</Link>
+          <Link to="/">🎨 Галерея</Link>
+        </nav>
+      </header>
+      <main className="main">{children}</main>
+    </div>
+  );
+}
+
+export default Layout;
